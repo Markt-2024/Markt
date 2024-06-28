382233cfa77e229a1481053fe6ad1f8e45d9d4c0
Rohith Thirunagari <trohith.2006@gmail.com>
2024-06-28 11:40:03 +0530

    added searchbar Component

---
+++ src/app/components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import "./SearchBar.css"
+export default function SearchBar() {
+    return(
+        <div className="SearchBar">
+            <div className="SearchBox">
+                <input>
+                </input>
+            </div>
+            <div className="SearchBtn">
+                Q
+            </div>
+        </div>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar/Navbar.js";
+import SearchBar from "./components/SearchBar/SearchBar.js";
 import "./globals.css"
 export default function Home() {
   return (
-    <div className="main">
+    <div>
       <Navbar/>
+      <SearchBar/>
     </div>
   );
 }
